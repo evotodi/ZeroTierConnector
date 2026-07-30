369f4794180d7d0754e5df1c10b235f2b20bc6c5
Evotodi <evotodi@gmail.com>
2026-07-30 14:26:23 -0400

    Add default SSH options to disable host key checking and user known hosts file

--- src/zerotier_connector/ssh.py
+++ src/zerotier_connector/ssh.py
@@ -7,33 +7,53 @@ from .models import ZeroTierAccount
 
 
 class SSHConnector:
+    def __init__(self):
+        self.sshDefaultOptions = [
+            "-o",
+            "StrictHostKeyChecking=no",
+            "-o",
+            "UserKnownHostsFile=/dev/null",
+        ]
+
     def Connect(self, account: ZeroTierAccount, ip: str) -> int:
         if account.ssh_key_path:
             sshCommand = [
                 "ssh",
                 "-i",
                 account.ssh_key_path,
+            ]
+
+            sshCommand.extend(self.sshDefaultOptions)
+            sshCommand.extend([
                 "-p",
                 str(account.ssh_port),
                 f"{account.ssh_username}@{ip}",
-            ]
+            ])
+
             return subprocess.call(sshCommand)
 
         if account.ssh_password:
+            # noinspection deprecation
             sshpassPath = shutil.which("sshpass")
             if not sshpassPath:
                 raise RuntimeError(
                     "sshpass is required for password auth. Install it or configure ssh_key_path."
                 )
+
             sshCommand = [
                 sshpassPath,
                 "-p",
                 account.ssh_password,
                 "ssh",
+            ]
+
+            sshCommand.extend(self.sshDefaultOptions)
+            sshCommand.extend([
                 "-p",
                 str(account.ssh_port),
                 f"{account.ssh_username}@{ip}",
-            ]
+            ])
+
             return subprocess.call(sshCommand)
 
         raise RuntimeError("No SSH authentication method configured.")
